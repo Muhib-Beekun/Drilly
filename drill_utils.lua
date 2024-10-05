7a79b6b68915ae5039c62e56906608d0e1e3a7df
Muhib Beekun <MBeekun@gmail.com>
2024-10-05 00:38:00 -0400

    Fix drill counting to prevent duplicate incrementing for resources

--- drill_utils.lua
+++ drill_utils.lua
@@ -42,10 +42,10 @@ end
 -- Function to fetch drill data for each resource and drill type
 function drill_utils.get_drill_data(surface)
     local drill_data = {}
+    local tracked_drills = {}
 
     -- Find all mining drills on the surface (dynamic type detection)
     local drills = surface.find_entities_filtered{type = "mining-drill"}
-    local tracked_drills = {}
 
     -- Loop through drills to associate them with mined resources
     for _, drill in pairs(drills) do
@@ -59,14 +59,20 @@ function drill_utils.get_drill_data(surface)
 
             -- Find all resources in the drill's mining area
             local resources = surface.find_entities_filtered{area = mining_area, type = "resource"}
+            local counted_resource_types = {}  -- Track resources this drill has been counted for
 
             for _, resource in pairs(resources) do
                 local resource_name = resource.name
-                if not drill_data[resource_name] then
-                    drill_data[resource_name] = {}
+
+                -- Track that the drill has mined this resource
+                if not counted_resource_types[resource_name] then
+                    if not drill_data[resource_name] then
+                        drill_data[resource_name] = {}
+                    end
+                    -- Increment the count of this drill type for this resource
+                    drill_data[resource_name][drill_type] = (drill_data[resource_name][drill_type] or 0) + 1
+                    counted_resource_types[resource_name] = true  -- Mark this resource as counted for this drill
                 end
-                -- Increment the count of this drill type for this resource
-                drill_data[resource_name][drill_type] = (drill_data[resource_name][drill_type] or 0) + 1
             end
 
             -- Mark this drill as counted
