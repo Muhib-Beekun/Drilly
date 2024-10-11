a4fd057e0dfd278c0bced30657b3bc68c799ec6b
Muhib Beekun <MBeekun@gmail.com>
2024-10-10 22:01:41 -0400

    Drilly button Create return in both cases

--- drilly_button.lua
+++ drilly_button.lua
@@ -1,3 +1,5 @@
+-- drilly_button.lua
+
 local mod_gui = require("mod-gui")
 
 local drilly_button = {}
@@ -5,10 +7,10 @@ local drilly_button = {}
 -- Create the Drilly button with a check if it already exists
 function drilly_button.create_drilly_button_if_needed(player)
     local button_flow = mod_gui.get_button_flow(player)
-    
+
     -- Check if the Drilly button already exists
     if not button_flow.drilly_button then
-        local button = button_flow.add{
+        local button = button_flow.add {
             type = "sprite-button",
             name = "drilly_button",
             sprite = "drilly_icon",
@@ -16,6 +18,8 @@ function drilly_button.create_drilly_button_if_needed(player)
             style = mod_gui.button_style
         }
         return button
+    else
+        return button_flow.drilly_button
     end
 end
 
